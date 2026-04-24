a95c1f788ab97b50722025bf52902439fdfc11a3
Sezer Can Ekiz <118104067+SezerCan17@users.noreply.github.com>
2026-04-24 15:05:37 +0300

    I created mvvm architecture and I made header UI.

---
+++ AYGUCI/Views/HeaderView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace AYGUCI.Views
+{
+    public partial class HeaderView : UserControl
+    {
+        public HeaderView()
+        {
+            InitializeComponent();
+        }
+    }
+}
