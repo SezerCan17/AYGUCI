da3c157f395115e2918929b9a233e5092c60ab83
Sezer Can Ekiz <118104067+SezerCan17@users.noreply.github.com>
2026-04-27 00:32:22 +0300

    Selected IHA and Notification panel added

---
+++ AYGUCI/Views/Selected_IHA_View.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AYGUCI.Views
+{
+    /// <summary>
+    /// Interaction logic for Selected_IHA_View.xaml
+    /// </summary>
+    public partial class Selected_IHA_View : UserControl
+    {
+        public Selected_IHA_View()
+        {
+            InitializeComponent();
+        }
+    }
+}
